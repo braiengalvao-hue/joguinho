8f22881e44ea4ffbe5221bd27cc5867773567dcc
Braien <braien.galvao@aluno.senai.br>
2026-03-18 15:50:41 -0300

    a

--- portal-arcade/routes/web.php
+++ portal-arcade/routes/web.php
@@ -16,4 +16,5 @@ Route::prefix('arcade')->group(function () {
     Route::get('/freefire', function () { return view('arcade.freefire'); })->name('arcade.freefire');
     // Coloque logo abaixo da rota do freefire
     Route::get('/clash', function () { return view('arcade.clash'); })->name('arcade.clash');   
+    Route::get('/rambo', function () { return view('arcade.rambo'); })->name('arcade.rambo');
 });
